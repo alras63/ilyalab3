b3fa9c2892991cb5716ec0a7348ef2c1d9b10060
ural1 <ural1@DESKTOP-EKUFV44>
2019-10-09 19:41:58 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string path = "D://текст.txt";
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(ofd.FileName);
+                string text = sr.ReadToEnd();
+
+                string[] predl = text.Split(new Char[] { '.' });
+                for (int i = predl.Length - 1; i >= 0; i--)
+                {
+                    Console.WriteLine(predl[i]);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Произошла ошибка");
+            }
+            Console.Read();
+
+        }
+    }
+}
